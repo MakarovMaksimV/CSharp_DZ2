66b5e574f29a422510da82a384cd6d13777de609
Maksim <mirror0791@mail.ru>
2024-08-14 22:18:45 +0300

    Initial commit

---
+++ DZ2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DZ2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+        int min = a[0, 0];
+
+        for(int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+
+            }
+        }
+
+        Console.ReadKey();
+
+        
+    }
+}
+
